4ba5c3a3d48f649299ffda69c41af7ad9e3d2b67
heitor-jsr <heitorscalon@gmail.com>
2022-05-09 21:20:56 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,9 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
+/* o código <Link to = {}`/form/${movie.id}`}> faz parte do bootstrap e serve
+para acessar o link de acordo com a variável em que o link está armazenado
 
+ */
 function MovieCard() {
     const movie = {
         id: 1,
@@ -14,7 +18,9 @@ function MovieCard() {
         <div className="dsmovie-card-bottom-container">
             <h3>{movie.title}</h3>
             <MovieScore />
-            <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            <Link to = {`/form/${movie.id}`}>
+                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            </Link>    
         </div>
     </div>
     );
