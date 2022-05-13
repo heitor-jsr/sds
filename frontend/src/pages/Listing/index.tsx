110057f625b7d8369accc9766ed16e20c08e4358
heitor-jsr <heitorscalon@gmail.com>
2022-05-13 13:53:52 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,19 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 
 
 function Listing() {
+    
+    //FORMA ERRADA - teste para fazer a requisição e ver se funciona.
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {console.log(response.data)}) 
+    //Com o código acima a nossa requisição já seria realizada. Porém, o get 
+    //do axios retorna um objeto promisses, que vai encapsular uma ação que pode 
+    //acontecer depois. Assim, para executar uma coisa depois que chegar a resposta da requisição,
+    //você precisa adicionar o ponto .then() e o que você passar de parametro é o que vai ser executado
+    //depois que a requisição retornar. O response é uma variável que vai receber uma lambda => com a 
+    //impressão do que o get retorna, com o .data. 
     return (
         /* Funções de componentes react só podem exportar um componente.
         para que dê certo a exportação de ambos, os componentes devem
